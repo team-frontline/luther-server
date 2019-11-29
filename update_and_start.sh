0b8ee49a17f0f286c2e796122026fe5ebff7a117
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-11-29 11:37:13 +0530

    git ignore updated;shell scrip for automatic pull: updated

--- update_and_start.sh
+++ update_and_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# shellcheck disable=SC2092
+# shellcheck disable=SC2006
 if ! `git status | grep -q "nothing to commit"`; then
   echo have diffs
 else
   echo no diffs
+  node server.js
 fi
